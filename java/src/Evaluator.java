e06e40f825834c2f700f77aeea3d6c4ab018ec2f
Thomas Ranvier <ranvier.thomas.pro@gmail.com>
2018-11-24 15:51:41 +0100

    Heuristics evaluation now handles stability

--- java/src/Evaluator.java
+++ java/src/Evaluator.java
@@ -5,6 +5,8 @@ import java.util.*;
  */
 public class Evaluator {
     private char player;
+    private int STABLE_INCREMENT = 8;
+    private float MOBILITY_WEIGHT = 0.85;
     private int[][] WEIGHTS = {{20, -3, 11, 8, 8, 11, -3, 20},
                                 {-3, -7, -4, -1, -1, -4, -7, -3},
                                 {11, -4, 4, 2, 2, 4, -1, 11},
@@ -13,15 +15,9 @@ public class Evaluator {
                                 {11, -4, 4, 2, 2, 4, -4, 11},
                                 {-3, -7, -4, -1, -1, -4, -7, -3},
                                 {20, -3, 11, 8, 8, 11, -3, 20}};
-    /*
-    private int[][] WEIGHTS = {{6, -3, 2, 2, 2, 2, -3, 6},
-                                {-3, -4, -1, -1, -1, -1, -4, -3},
-                                {2, -1, 1, 0, 0, 1, -1, 2},
-                                {2, -1, 0, 1, 1, 0, -1, 2},
-                                {2, -1, 0, 1, 1, 0, -1, 2},
-                                {2, -1, 1, 0, 0, 1, -1, 2},
-                                {-3, -4, -1, -1, -1, -1, -4, -3},
-                                {6, -3, 2, 2, 2, 2, -3, 6}}; */ /**
+                                {6, -3, 2, 2, 2, 2, -3, 6}};
+
+    /**
      * Class constructor that takes the played player as argument.
      * @param player The player that the bot plays.
      */
@@ -36,78 +32,111 @@ public class Evaluator {
      */
     public int evaluate(Position position) {
         return componentWiseHeuristic(position);
-        //return staticWeightsHeuristic(position);
     }
     
     /**
-     * That function uses constant weights as value for every cell of the 
-     * grid to compute the heuristic of the game state.
+     * That function returns an evaluation value of the board which uses the static
+     * weights and increases the value if the cell is stable.
      * @param position The actual position of the game.
      * @return A value higher if the player is in a good position, lower otherwise.
      */
-    private int staticWeightsHeuristic(Position position) {
+    private int staticWeightsAndStabilityHeuristic(Position position) {
         char[][] grid = position.getGrid();
         int result = 0;
         for(int x = 0; x < position.SIZE; x++) {
             for(int y = 0; y < position.SIZE; y++) {
                 if (grid[x][y] != 'E') {
-                    result += grid[x][y] == this.player ? this.WEIGHTS[x][y] : -this.WEIGHTS[x][y];
+                    int cell_value = this.cellEvaluation(grid, position.SIZE, x, y);
+                    result += grid[x][y] == this.player ? cell_value : -cell_value;
                 }
             }
         }
         return result;
     }
 
-    private int componentWiseHeuristic(Position position) {
-        int coins = staticWeightsHeuristic(position);
-        int moves = actualMobilityHeuristic(position);
-        int stability = 0;//stabilityHeuristic(position);
-        return coins + (int)Math.round(0.7 * moves) + stability;
+    /**
+     * That function returns a computed value for the selected cell of the grid, that value
+     * is determined from the static weights and the value is incremented by a bonus if the cell
+     * is considered as stable.
+     * @param grid The grid of the game at its actual state
+     * @param size The size of the grid
+     * @param x Coordinate x
+     * @param x Coordinate y
+     * @return The computed value of the cell
+     */
+    private int cellEvaluation(char[][] grid, int size, int x, int y) {
+        int cell_value = this.WEIGHTS[x][y];
+        if (isStableCell(grid, size, x, y))
+            cell_value = cell_value < 0 ? this.STABLE_INCREMENT : cell_value + this.STABLE_INCREMENT;
+        return cell_value;
     }
 
+    /**
+     * That function evaluates if the selected cell can be considered as stable for the next move or not,
+     * that means that it returns false if the cell can potentially be captured the direct next move
+     * and true otherwise.
+     * @param grid The grid of the game at its actual state
+     * @param size The size of the grid
+     * @param x Coordinate x
+     * @param x Coordinate y
+     * @return A boolean true if the cell is stable, false otherwise
+     */
+    private boolean isStableCell(char[][] grid, int size, int x, int y) {
+        for (int dir_x = -1; dir_x <= 1; dir_x++) {
+            for (int dir_y = -1; dir_y <= 1; dir_y++) {
+                int new_x = x + dir_x, new_y = y + dir_y;
+                if (this.isInGrid(new_x, new_y, size)) {
+                    if (grid[new_x][new_y] == 'E') {
+                        int opp_dir_x = dir_x * -1, opp_dir_y = dir_y * -1;
+                        new_x = x + opp_dir_x;
+                        new_y = y + opp_dir_y;
+                        while (isInGrid(new_x, new_y, size) && grid[new_x][new_y] == grid[x][y]) {
+                            new_x += opp_dir_x;
+                            new_y += opp_dir_y;
+                        }
+                        if (isInGrid(new_x, new_y, size))
+                            return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * That function simply returns true if the selected cell is in the grid boundaries.
+     * @param x Coordinate x
+     * @param x Coordinate y
+     * @param size The size of the grid
+     * @return A boolean true if the cell is in the grid, false otherwise
+     */
+    private boolean isInGrid(int x, int y, int size) {
+        return x >= 0 && x < size && y >= 0 && y < size;
+    }
+
+    /**
+     * That function gets the different heuristics values and weights them to return a balanced
+     * result.
+     * @param position The actual position of the game
+     * @return The weighted heuristics value
+     */
+    private int componentWiseHeuristic(Position position) {
+        int coins = staticWeightsAndStabilityHeuristic(position);
+        int moves = actualMobilityHeuristic(position);
+        return coins + (int)Math.round(this.MOBILITY_WEIGHT * moves);
+    }
+
+    /**
+     * That function evaluates the possible mobility of the player in regard to the opponent
+     * possible mobility.
+     * @param position The actual position of the game
+     * @return A value higher if the player is in a good position, lower otherwise
+     */
     private int actualMobilityHeuristic(Position position) {
         ArrayList<Move> own_valid_moves = position.getValidMoves();
         position.changePlayer();
         ArrayList<Move> enemy_valid_moves = position.getValidMoves();
         position.changePlayer();
         return own_valid_moves.size() - enemy_valid_moves.size();
-    }
-
-    private int stabilityHeuristic(Position position) {
-        char[][] grid = position.getGrid();
-        int own_stables = 0, enemy_stables = 0, size = position.SIZE;
-        if (grid[0][0] != 'E' || grid[size - 1][0] != 'E' || grid[0][size - 1] != 'E' || grid[size - 1][size - 1] != 'E') {
-            own_stables = totalStability(grid, size, position.getPlayer());
-            enemy_stables = totalStability(grid, size, position.getPlayer() == 'X' ? 'O' : 'X');
-        }
-        return own_stables - enemy_stables;
-    }
-
-    private int totalStability(char[][] grid, int size, char player) {
-        Set<ArrayList<Integer>> stable_coins = new HashSet<ArrayList<Integer>>();
-        stabilityFromCorner(stable_coins, grid, size, player, 0, 0, 1, 1);
-        stabilityFromCorner(stable_coins, grid, size, player, size - 1, 0, -1, 1);
-        stabilityFromCorner(stable_coins, grid, size, player, 0, size - 1, 1, -1);
-        stabilityFromCorner(stable_coins, grid, size, player, size - 1, size - 1, -1, -1);
-        return stable_coins.size();
-    }
-
-    private void stabilityFromCorner(Set<ArrayList<Integer>> stable_coins, char[][] grid, int size, char player, int x, int y, int dir_x, int dir_y) {
-        int mem_y = y;
-        for (int i = 0; i < size; i++) {
-            y = mem_y;
-            for (int j = 0; j < size; j++) {
-                if (grid[x][y] == player) {
-                    ArrayList<Integer> pair = new ArrayList<>();
-                    pair.add(x);
-                    pair.add(y);
-                    stable_coins.add(pair);
-                } else {
-                    break;
-                }
-                y += dir_y;
-            }
-            x += dir_x;
-        }
     }
 }
